31d0be8073fa74dd4a95da11de52c1ba2cb50e3e
Paul Williams <pgwilliams107@gmail.com>
2020-05-07 21:07:16 -0500

    commit before bonus tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,7 +1,7 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
@@ -9,8 +9,66 @@ public class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
-        assertEquals(true, true);
+        assertTrue(true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void stringInsideBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void stringInsideBracketsWithSeparateStringReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void stringWithSeparateEmptyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void noClosingBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void bracketsInReverseOrderReturnsFalse(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void oneSidedBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void onlyBracketsInReverseOrderReturnsFalse(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void escapeCharactersReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[\n]"));
+    }
+
+    @Test
+    public void balancedBracketsWithNonBalancedBracketsReturnsFalse(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]]["));
+    }
+
+    @Test
+    public void nonEscapeCharactersReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[@#word23]"));
+    }
 }
---
+++ src/test/BonusBinarySearchTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.Test;
+import main.BonusBinarySearch;
+import static org.junit.Assert.*;
+
+public class BonusBinarySearchTest {
+
+    @Test
+    public void emptyTest(){
+        assertEquals(-1,-1,.001);
+    }
+}
